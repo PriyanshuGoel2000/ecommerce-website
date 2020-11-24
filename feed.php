513889c0dd5946d877d40bc437c8f16786dfd3e1
Priyanshu Goel <55882904+PriyanshuGoel2000@users.noreply.github.com>
2020-11-24 17:41:51 +0530

    Register Updated
    No same email can register more than once

--- feed.php
+++ feed.php
@@ -8,26 +8,42 @@ $Phone=$_POST['Phone']." ";
 $Address=$_REQUEST['Address']." ";
 $City=$_REQUEST['City']." ";
 $Pin=$_REQUEST['Pin']." ";
-$query="select Email from user_detail where Email=".$email;
+$query="select Email from user_detail where Email='$email'";
 $result=mysqli_query($conn,$query);
 $count=mysqli_num_rows($result);
 if($count>0)
 {
+	echo "<br>";
 	echo "Already Registered";
-	echo "Registered";
+	header("refresh:3; url = ecom_login.html");
 }
+	
 else{
 
 $sql="INSERT into user_detail(Fname, Lname, Email, DOB, Phone, Address, Pin, City) VALUES('$Fname', '$Lname','$email','$dob','$Phone','$Address', '$Pin', '$City')";
 
 
 mysqli_query($conn,$sql);
+echo "<br>";
 echo "Registered";
-header("refresh:2; url = ecom_login.html");
+header("refresh:3; url = ecom_login.html");
 }
+/*
+$query = "select Email from user_detail where Email='$email'";
+$result=mysqli_query($conn,$query);
+$count=mysqli_num_rows($result);
+if($count>0){
+	  ?>
+ <script>
+ alert("you are already registered");
+ echo "Already Registered";
+ window.location="razor.php";
+ </script>
+ <?php }
+
+*/
+
 
 
 ?>
-
-
 
