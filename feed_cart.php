6c28b526e9705972fbb91a85d0efc0e76dbef1c4
Priyanshu Goel <55882904+PriyanshuGoel2000@users.noreply.github.com>
2020-12-15 15:56:20 +0530

    cart updated

--- feed_cart.php
+++ feed_cart.php
@@ -1,15 +1,21 @@
 <?php
+<<<<<<< HEAD
+session_start();
 include "conn.php";
-$tag=$_REQUEST['tag']." ";
-echo $tag;
-$result1 = mysqli_query($conn, "SELECT * FROM product_detail WHERE tag=$tag ");
-$row = mysqli_fetch_array($result1);
-$img=$row['image'];
-$id=$row['tag'];
-$name=$row['name'];
-$price=$row['tag'];
-echo $sql="INSERT into cart(image, id, name, price) VALUES('$img', '$id','$name','$price')";
+echo $tag=$_GET['tag'];
+echo $price=$_GET['price'];
+echo $img=$_GET['image'];
+$sql="INSERT into cart(image, name, price) VALUES('$img', '$tag','$price')";
 mysqli_query($conn,$sql);
+header("refresh:0; url = products.php");
+/*
+$result1 = mysqli_query($conn, "SELECT * FROM product_detail WHERE tag=$tag ");
+//$row = mysqli_fetch_array($result1);
 
-?>
+//$id=$_GET['tag'];
+echo $name=$_GET['name'];
+echo $price=$_GET['price'];
+//echo 
+*/
+
 
