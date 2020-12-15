850ee34bd963711dbc11934e6f1452b6e47ea30f
Priyanshu Goel <55882904+PriyanshuGoel2000@users.noreply.github.com>
2020-12-15 15:52:25 +0530

    cart corrected

---
+++ feed_cart.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include "conn.php";
+echo $tag=$_GET['tag'];
+echo $price=$_GET['price'];
+echo $img=$_GET['image'];
+$sql="INSERT into cart(image, name, price) VALUES('$img', '$tag','$price')";
+mysqli_query($conn,$sql);
+header("refresh:0; url = products.php");
+/*
+$result1 = mysqli_query($conn, "SELECT * FROM product_detail WHERE tag=$tag ");
+//$row = mysqli_fetch_array($result1);
+
+//$id=$_GET['tag'];
+echo $name=$_GET['name'];
+echo $price=$_GET['price'];
+//echo 
+*/
+?>
+
